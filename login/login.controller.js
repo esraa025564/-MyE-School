34117a30143bdf5f48c6fa07e40268c482213f5b
esraa025564 <esraa.mohamed@ruptela.com>
2017-02-25 23:30:23 +0200

    adding
    a

---
+++ login/login.controller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var viewModel = this;
+
+        viewModel.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            viewModel.dataLoading = true;
+            AuthenticationService.Login(viewModel.username, viewModel.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(viewModel.username, viewModel.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    viewModel.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
