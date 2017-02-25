8f5bea7b94c09e808a4a77d792ec702ebe0c5eb4
esraa025564 <esraa.mohamed@ruptela.com>
2017-02-25 23:21:06 +0200

    Add files via upload

---
+++ register.controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function RegisterController(UserService, $location, $rootScope, FlashService) {
+        var viewModel = this;
+
+        viewModel.register = register;
+
+        function register() {
+            viewModel.dataLoading = true;
+            UserService.Create(viewModel.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        viewModel.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
